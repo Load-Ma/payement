e0dbf76cbcc33484827346dba19fb0ec2d9a2b98
Romain <romain.caille@epsi.com>
2021-11-23 08:11:43 +0100

    Ajoutez des fichiers projet.

---
+++ payement_DAL/Depot_DAL.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payement.DAL
+{
+    public abstract class Depot_DAL<Type_DAL> : IDepot_DAL<Type_DAL>
+    {
+        public string ConnectionString { get; set; }
+
+        protected SqlConnection connection;
+        protected SqlCommand command;
+        public Depot_DAL()
+        {
+            var builder = new ConfigurationBuilder();
+            var config = builder.AddJsonFile("appsettings.json", false, true).Build();
+
+            ConnectionString = config.GetSection("ConnectionStrings:default").Value;
+        }
+
+        protected void CreateConnectionAndCommand()
+        {
+            connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            command = new SqlCommand();
+            command.Connection = connection;
+        }
+
+        protected void DestroyConnectionAndCommand()
+        {
+            command.Dispose();
+            connection.Close();
+            connection.Dispose();
+        }
+
+        #region abstract methode
+        public abstract void Delete(Type_DAL item);
+        public abstract List<Type_DAL> GetAll();
+        public abstract Type_DAL GetByID(int ID);
+        public abstract Type_DAL Insert(Type_DAL item);
+        public abstract Type_DAL Update(Type_DAL item);
+        #endregion
+    }
+}
---
+++ payement_DAL/User_DAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payement.DAL
+{
+    class User_DAL
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User_DAL(string username, string email) => (Username, Email) = (username, email);
+
+        public User_DAL(int id, string username, string email, string password)
+            => (ID, Username, Email, Password) = (id, username, email, password);
+
+        public string HashedPassword(string password)
+        {
+            // todo : hashpassword
+            return Password;
+        }
+
+        internal void Insert(SqlConnection connection)
+        {
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "insert into users(username, email, password)"
+                    + " values (@username, @email, @password)";
+                command.Parameters.Add(new SqlParameter("@username", Username));
+                command.Parameters.Add(new SqlParameter("@email", Email));
+                command.Parameters.Add(new SqlParameter("@password", HashedPassword(Password)));
+                command.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
