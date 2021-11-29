232fd516d6c622285460d23be89e1e8e12cb9ba6
Romain <romain.caille@epsi.com>
2021-11-29 18:53:39 +0100

    début de la couche métier

---
+++ tp_csharp/Guest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payement
+{
+    public class Guest
+    {
+        public int ID { get; set; }
+        public string Party_ID { get; set; }
+        public string Username { get; set; }
+        public string Spent { get; set; }
+
+
+        public Guest(string party_id, string username, string spent)
+        {
+            Party_ID = party_id;
+            Username = username;
+            Spent = spent;
+        }
+
+        public Guest(int id, string party_id, string username, string spent)
+        {
+            ID = id;
+            Party_ID = party_id;
+            Username = username;
+            Spent = spent;
+        }
+    }
+}
---
+++ tp_csharp/Party.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payement
+{
+    public class Party
+    {
+        public string ID { get; set; }
+        public int Organisator_ID { get; set; }
+        public string Name { get; set; }
+
+        public Party(int organisator_id, string name)
+        {
+            ID = GenerateUID();
+            Organisator_ID = organisator_id;
+            Name = name;
+        }
+
+        public Party(string id, int organisator_id, string name)
+        {
+            ID = id;
+            Organisator_ID = organisator_id;
+            Name = name;
+        }
+
+        private string GenerateUID()
+        {
+            Guid g = Guid.NewGuid();
+
+            return g.ToString();
+        }
+    }
+}
